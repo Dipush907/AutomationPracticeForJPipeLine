edb0a90a7443d7f63718d68081c5d8f41935fa5e
bibhudatta <dipush907@gmail.com>
2022-11-29 12:11:28 +0530

    This automation practice project is to be run in jenkins pipeline.

---
+++ src/main/java/pageRepo/BaseClass.java
@@ -0,0 +1,38 @@
+package pageRepo;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class BaseClass {
+	
+	public static WebDriver driver;
+	
+	@BeforeSuite
+	public void openBrowser()
+	{
+		ChromeOptions opt = new ChromeOptions();
+		opt.addArguments("--headless");
+		WebDriverManager.chromedriver().setup();
+		driver= new ChromeDriver(opt);
+	}
+	
+	@BeforeTest
+	public void openURL()
+	{
+		
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+		driver.manage().window().maximize();
+		driver.get("http://automationpractice.com");
+	}
+	
+}
